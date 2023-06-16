251dfb8c21410ebb4586ad16996da04dc1b54e31
FewerElk <120582916+FewerElk@users.noreply.github.com>
2023-06-16 20:01:25 +0200

    Update Listener.java

--- fr/fewerelk/eventplugin/Listener.java
+++ fr/fewerelk/eventplugin/Listener.java
@@ -1,10 +1,14 @@
 package fr.fewerelk.eventplugin;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.Bukkit;
 
-public class Listener implements Listener {}
+public class Listener implements Listener {
+
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupEvent e) {
+        Bukkit.getLogger().info("Player " + e.getPlayer() + " get an item")
+    }
+}
